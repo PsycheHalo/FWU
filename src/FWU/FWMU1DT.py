15c05bce6fea74eb9e9283189cb1be44c44210ef
PsycheHalo <psyche@winsgod.com>
2025-07-30 15:38:33 +0800

    Update FWMU1DT.py

--- src/FWU/FWMU1DT.py
+++ src/FWU/FWMU1DT.py
@@ -2,19 +2,35 @@ import torch
 import math
 
 class FWMU1DT(torch.nn.Module):
-    def __init__(self,in_channels,out_channels,kernel_size,stride=1,padding=0,output_padding=0,dilation=1,groups=1,padding_mode='zeros',bias=True,gain=1,**kwargs):
+    def __init__(self,in_channels,out_channels,kernel_size,stride=1,padding=0,output_padding=0,dilation=1,groups=1,padding_mode='zeros',bias=True,gain=1,kernel_size_N=None,stride_N=None,padding_N=None,output_padding_N=None,dilation_N=None,groups_N=None,padding_mode_N=None,**kwargs):
         super(FWMU1DT, self).__init__()
+        if kernel_size_N is None:
+            kernel_size_N=kernel_size
+        if stride_N is None:
+            stride_N=stride
+        if padding_N is None:
+            padding_N=padding
+        if output_padding_N is None:
+            output_padding_N=output_padding
+        if dilation_N is None:
+            dilation_N=dilation
+        if groups_N is None:
+            groups_N=groups
+        if padding_mode_N is None:
+            padding_mode_N=padding_mode
+            
         self.ConvP=torch.nn.ConvTranspose1d(in_channels,out_channels,kernel_size,stride=stride,padding=padding,output_padding=output_padding,dilation=dilation,groups=groups,padding_mode=padding_mode,bias=True,**kwargs)
-        self.ConvN=torch.nn.ConvTranspose1d(in_channels,out_channels,kernel_size,stride=stride,padding=padding,output_padding=output_padding,dilation=dilation,groups=groups,padding_mode=padding_mode,bias=True,**kwargs)
+        self.ConvN=torch.nn.ConvTranspose1d(in_channels,out_channels,kernel_size_N,stride=stride_N,padding=padding_N,output_padding=output_padding_N,dilation=dilation_N,groups=groups_N,padding_mode=padding_mode_N,bias=True,**kwargs)
         
         if bias:
             self.bias=torch.nn.Parameter(torch.zeros((out_channels,1),**kwargs),requires_grad=True)
         else:
             self.bias=None
-              
+
+        g=math.sqrt(math.sqrt(1+gain**2)-1)
         with torch.no_grad():
-            torch.nn.init.orthogonal_(self.ConvP.weight,gain=gain)
-            torch.nn.init.orthogonal_(self.ConvN.weight,gain=gain)
+            torch.nn.init.orthogonal_(self.ConvP.weight,gain=g)
+            torch.nn.init.orthogonal_(self.ConvN.weight,gain=g)
             torch.nn.init.normal_(self.ConvP.bias)
             self.ConvP.bias.data.sign_()
             torch.nn.init.normal_(self.ConvN.bias)
