66f84c6acb2a0260ec29ae454536c2b7e971fd53
PsycheHalo <psyche@winsgod.com>
2025-08-29 15:29:46 +0800

    Update FWMU1DT.py

--- src/FWU/FWMU1DT.py
+++ src/FWU/FWMU1DT.py
@@ -6,7 +6,7 @@ except ImportError:
     from transOrthogonal_ import transOrthogonal_
 
 class FWMU1DT(torch.nn.Module):
-    def __init__(self,in_channels,out_channels,kernel_size,stride=1,padding=0,output_padding=0,dilation=1,groups=1,padding_mode='zeros',bias=True,gain=1,kernel_size_N=None,stride_N=None,padding_N=None,output_padding_N=None,dilation_N=None,groups_N=None,padding_mode_N=None,**kwargs):
+    def __init__(self,in_channels,out_channels,kernel_size,stride=1,padding=0,output_padding=0,dilation=1,groups=1,padding_mode='zeros',bias=True,gain=1,kernel_size_N=None,stride_N=None,padding_N=None,output_padding_N=None,dilation_N=None,groups_N=None,padding_mode_N=None,gain_N=None,**kwargs):
         super(FWMU1DT, self).__init__()
         if kernel_size_N is None:
             kernel_size_N=kernel_size
@@ -22,6 +22,8 @@ class FWMU1DT(torch.nn.Module):
             groups_N=groups
         if padding_mode_N is None:
             padding_mode_N=padding_mode
+        if gain_N is None:
+            gain_N=gain
             
         self.ConvP=torch.nn.ConvTranspose1d(in_channels,out_channels,kernel_size,stride=stride,padding=padding,output_padding=output_padding,dilation=dilation,groups=groups,padding_mode=padding_mode,bias=True,**kwargs)
         self.ConvN=torch.nn.ConvTranspose1d(in_channels,out_channels,kernel_size_N,stride=stride_N,padding=padding_N,output_padding=output_padding_N,dilation=dilation_N,groups=groups_N,padding_mode=padding_mode_N,bias=True,**kwargs)
@@ -32,9 +34,10 @@ class FWMU1DT(torch.nn.Module):
             self.bias=None
 
         g=math.sqrt(math.sqrt(1+gain**2)-1)
+        g_N=math.sqrt(math.sqrt(1+gain_N**2)-1)
         with torch.no_grad():
             transOrthogonal_(self.ConvP.weight,gain=g)
-            transOrthogonal_(self.ConvN.weight,gain=g)
+            transOrthogonal_(self.ConvN.weight,gain=g_N)
             torch.nn.init.normal_(self.ConvP.bias)
             self.ConvP.bias.data.sign_()
             torch.nn.init.normal_(self.ConvN.bias)
@@ -49,4 +52,5 @@ class FWMU1DT(torch.nn.Module):
             output=output+self.bias
         
         return output
+
   
